389d0f9f532e741844bd8290367931ecd1efb52c
Anatolii-melashchenko <toramulo2012@gmail.com>
2022-12-14 22:47:46 +0200

    second commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+const DELAY = 1000;
+let timerId = null;
 
+refs.start.addEventListener('click', onChangeColorStart);
+refs.stop.addEventListener('click', onChangeColorStop);
+
+function onChangeColorStart() {
+  timerId = setInterval(() => {
+    let randomColor = getRandomHexColor();
+
+    refs.body.style.backgroundColor = randomColor;
+    refs.start.setAttribute('disabled', true);
+  }, DELAY);
+}
+
+function onChangeColorStop() {
+  clearInterval(timerId);
+
+  refs.start.removeAttribute('disabled');
+}
